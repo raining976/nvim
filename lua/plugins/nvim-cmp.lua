f2cfcf78d41f4557f184c8a77ce99a66a76e2c9e
raining <running.zhang@foxmail.com>
2024-04-10 19:41:45 +0800

    添加autopairs插件

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -8,13 +8,10 @@ local M = {
 	{
 		"hrsh7th/nvim-cmp",
 		dependencies = { "lspkind.nvim" },
-		config = function()
-			require("config.nvim-cmp")
-		end,
 	},
 	{ "hrsh7th/cmp-nvim-lsp", dependencies = { "nvim-cmp" } },
 	{ "hrsh7th/cmp-buffer", dependencies = { "nvim-cmp" } }, -- buffer auto-completion
-	{ "hrsh7th/cmp-path", dependencies = { "nvim-cmp" } }, -- path auto-completion
+	{ "hrsh8th/cmp-path", dependencies = { "nvim-cmp" } }, -- path auto-completion
 	{ "hrsh7th/cmp-cmdline", dependencies = { "nvim-cmp" } }, -- cmdline auto-completion
 	-- Code snippet engine
 	{
